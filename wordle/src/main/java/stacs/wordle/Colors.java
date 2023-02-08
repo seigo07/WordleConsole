b88a91af57f1847e7f8ae2941d16a06d6e449520
seigo07 <seven_8s@icloud.com>
2023-02-08 23:45:12 +0000

    Create Colors enum and ColorsTest

---
+++ wordle/src/main/java/stacs/wordle/Colors.java
@@ -0,0 +1,4 @@
+package stacs.wordle;
+
+public enum Colors {
+}
---
+++ wordle/src/test/java/stacs/wordle/ColorsTest.java
@@ -0,0 +1,19 @@
+package stacs.wordle;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+}
