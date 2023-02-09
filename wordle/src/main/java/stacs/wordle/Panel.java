d1c489b6837a98b9e885a6ae9cfba7e5f1d89577
seigo07 <seven_8s@icloud.com>
2023-02-09 00:23:42 +0000

    Implement Panel Class with PanelTest

--- wordle/src/main/java/stacs/wordle/Panel.java
+++ wordle/src/main/java/stacs/wordle/Panel.java
@@ -1,4 +1,53 @@
 package stacs.wordle;
 
+/**
+ * Panel
+ * @author Seigo
+ */
 public class Panel {
+
+    private char letter;
+    private Colors color;
+
+    /**
+     * constructor
+     * @param
+     */
+    public Panel() {
+        this.letter = ' ';
+        this.color = Colors.GRAY;
+    }
+
+    /**
+     * getter letter
+     * @return letter
+     */
+    public char getLetter() {
+        return letter;
+    }
+
+    /**
+     * getter color
+     * @return color
+     */
+    public Colors getColor() {
+        return color;
+    }
+
+    /**
+     * setter letter
+     * @param letter
+     */
+    public void setLetter(char letter) {
+        this.letter = letter;
+    }
+
+    /**
+     * setter color
+     * @param color
+     */
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
 }
--- wordle/src/test/java/stacs/wordle/PanelTest.java
+++ wordle/src/test/java/stacs/wordle/PanelTest.java
@@ -2,9 +2,14 @@ package stacs.wordle;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * PanelTest
+ * @author Seigo
+ */
 class PanelTest {
 
     @BeforeEach
@@ -14,4 +19,51 @@ class PanelTest {
     @AfterEach
     void tearDown() {
     }
+
+    /**
+     * Get a initial letter.
+     * @result Test letter is set ' ' after initialization.
+     */
+    @Test
+    public void shouldGetInitialLetter()
+    {
+        Panel panel = new Panel();
+        assertEquals(' ', panel.getLetter());
+    }
+
+    /**
+     * Get a initial color.
+     * @result Test color is set GRAY after initialization.
+     */
+    @Test
+    public void shouldGetInitialColor()
+    {
+        Panel panel = new Panel();
+        assertEquals(Colors.GRAY, panel.getColor());
+    }
+
+    /**
+     * Set a letter.
+     * @result Letter should be 'A' after being called setLetter with 'A'.
+     */
+    @Test
+    public void shouldSetLetter()
+    {
+        Panel panel = new Panel();
+        panel.setLetter('A');
+        assertEquals('A', panel.getLetter());
+    }
+
+    /**
+     * Set a Color.
+     * @result Color should be YELLOW after being called setColor with YELLOW.
+     */
+    @Test
+    public void shouldSetColor()
+    {
+        Panel panel = new Panel();
+        panel.setColor(Colors.YELLOW);
+        assertEquals(Colors.YELLOW, panel.getColor());
+    }
+
 }
