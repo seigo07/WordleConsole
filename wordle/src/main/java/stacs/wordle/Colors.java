628f3ee47b183f4affab27a8f9c9748cceef8d2d
seigo07 <seven_8s@icloud.com>
2023-02-09 00:08:42 +0000

    Implement Colors Class with ColorsTest

--- wordle/src/main/java/stacs/wordle/Colors.java
+++ wordle/src/main/java/stacs/wordle/Colors.java
@@ -1,4 +1,33 @@
 package stacs.wordle;
 
+/**
+ * Colors
+ * @author Seigo
+ */
 public enum Colors {
+
+    YELLOW("\u001b[43;1m"),
+    GREEN("\u001b[42;1m"),
+    GRAY("\u001b[40;1m"),
+    WHITE("\033[0m"),
+    ;
+
+    private String code;
+
+    /**
+     * constructor
+     * @param code
+     */
+    Colors(String code) {
+        this.code = code;
+    }
+
+    /**
+     * getter code
+     * @return code
+     */
+    public String getCode() {
+        return this.code;
+    }
+
 }
--- wordle/src/test/java/stacs/wordle/ColorsTest.java
+++ wordle/src/test/java/stacs/wordle/ColorsTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * ColorsTest
+ * @author Seigo
+ */
 class ColorsTest {
 
     @BeforeEach
@@ -14,6 +18,50 @@ class ColorsTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    /**
+     * Get a yellow color code.
+     * @result Test whether YELLOW has "\u001b[43;1m" color code or not.
+     */
+    @Test
+    public void shouldGetYellowCode()
+    {
+        Colors yellowColor = Colors.YELLOW;
+        assertEquals("\u001b[43;1m", yellowColor.getCode());
+    }
+
+    /**
+     * Get a green color code.
+     * @result Test whether GREEN has "[42;1m" color code or not.
+     */
+    @Test
+    public void shouldGetGreenCode()
+    {
+        Colors greenColor = Colors.GREEN;
+        assertEquals("\u001b[42;1m", greenColor.getCode());
+    }
+
+    /**
+     * Get a gray code.
+     * @result Test whether GRAY has "\u001b[40;1m" color code or not.
+     */
+    @Test
+    public void shouldGetGrayCode()
+    {
+        Colors grayColor = Colors.GRAY;
+        assertEquals("\u001b[40;1m", grayColor.getCode());
+    }
+
+    /**
+     * Get a white code.
+     * @result Test whether WHITE has "\u001b[40;1m" color code or not.
+     */
+    @Test
+    public void shouldGetWhiteCode()
+    {
+        Colors whiteColor = Colors.WHITE;
+        assertEquals("\033[0m", whiteColor.getCode());
     }
 
 }
