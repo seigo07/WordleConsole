a82b4c8b7d4771648c02eae3db73c63513ec9aae
seigo07 <seven_8s@icloud.com>
2023-02-09 23:29:08 +0000

    move loadWordlist from WordleApp to WordleManager and remove it from WordleAppTest

--- wordle/src/main/java/stacs/wordle/WordleApp.java
+++ wordle/src/main/java/stacs/wordle/WordleApp.java
@@ -7,24 +7,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.nio.file.Path;
 
-public class WordleApp
-{
-    public static void main( String[] args )
-    {
+public class WordleApp {
+    public static void main(String[] args) {
         System.out.println("Welcome to CS5031 - Wordle");
-    }
-
-    // Unimplemented skeleton
-    // You may refactor this method
-    protected static ArrayList<String> loadWordlist(String wordlistPath) throws FileNotFoundException
-    {
-        Scanner s = new Scanner(new File(wordlistPath));
-        ArrayList<String> wordlist = new ArrayList<String>();
-        while (s.hasNext()){
-            wordlist.add(s.next());
-        }
-        s.close();
-
-        return wordlist;
     }
 }
--- wordle/src/test/java/stacs/wordle/WordleAppTest.java
+++ wordle/src/test/java/stacs/wordle/WordleAppTest.java
@@ -8,14 +8,5 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class WordleAppTest
-{
-    @Test
-    public void shouldLoadWordlist() throws FileNotFoundException
-    {
-        ArrayList<String> wordlist = WordleApp.loadWordlist("src/test/resources/wordlist-test.txt");
-
-        // test wordlist only contains 3 words, so wordlist should have the size of 3
-        assertEquals(3, wordlist.size());
-    }
+public class WordleAppTest {
 }
