02099bdf3368ffdad69cd8b21cde25ea1bd56a7d
seigo07 <seven_8s@icloud.com>
2023-02-09 23:27:37 +0000

    Implement WordleManager Class with WordleManagerTest

--- wordle/src/main/java/stacs/wordle/WordleManager.java
+++ wordle/src/main/java/stacs/wordle/WordleManager.java
@@ -1,4 +1,196 @@
 package stacs.wordle;
 
-public class WordleManager {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+/**
+ * WordleManager
+ * Wordle game management class
+ *
+ * @author Seigo
+ */
+class WordleManager {
+
+    private static final String WORDLIST_FILE_PATH = "src/main/resources/wordlist.txt";
+    private static final String SUCCESS_MESSAGE = "Congratulations! Your score is ";
+    private static final String FAILURE_MESSAGE = "Failure. The correct answer is ";
+    private static final int LIMIT_NUMBER_OF_LETTERS = 5;
+    private static final int LIMIT_NUMBER_OF_ATTEMPTS = 6;
+
+    private String answer;
+    private String outputText;
+    private ArrayList<String> words;
+    private LinkedHashMap<Character, Integer> letters;
+    private int numberOfAttempts;
+
+    /**
+     * constructor
+     */
+    public WordleManager(String filePath) throws FileNotFoundException {
+        words = loadWordlist(filePath);
+        setAnswer();
+        setLetters();
+    }
+
+    /**
+     * Load text file from given path.
+     * Read each word line by line and add it to String list.
+     *
+     * @return word list loaded from given path.
+     */
+    protected static ArrayList<String> loadWordlist(String wordlistPath) throws FileNotFoundException {
+        Scanner s = new Scanner(new File(wordlistPath));
+        ArrayList<String> wordlist = new ArrayList<>();
+        while (s.hasNext()) {
+            wordlist.add(s.next().toUpperCase());
+        }
+        s.close();
+        return wordlist;
+    }
+
+    /**
+     * setter answer
+     * Get an answer from answers randomly (from index of Math.random() * answers.size())
+     * Set it to answer.
+     */
+    private void setAnswer() {
+        answer = words.get((int) (Math.random() * words.size()));
+    }
+
+    /**
+     * Organize letters with each character and its numbers based on NUMBER_OF_LETTERS.
+     */
+    private void setLetters() {
+        letters = new LinkedHashMap<>();
+        int k;
+        for (int i = 0; i < WordleManager.getLimitNumberOfLetters(); i++) {
+            k = 1;
+            for (int j = 0; j < WordleManager.getLimitNumberOfLetters(); j++) {
+                if (i != j && answer.charAt(i) == answer.charAt(j))
+                    k++;
+            }
+            letters.put(answer.charAt(i), k);
+        }
+    }
+
+    /**
+     * getter answer
+     *
+     * @return answer
+     */
+    public String getAnswer() {
+        return answer;
+    }
+
+    /**
+     * getter outputText
+     *
+     * @return outputText
+     */
+    public String getOutputText() {
+        return outputText;
+    }
+
+    /**
+     * getter words
+     *
+     * @return words
+     */
+    public ArrayList<String> getWords() {
+        return words;
+    }
+
+    /**
+     * getter letters
+     *
+     * @return letters
+     */
+    public LinkedHashMap<Character, Integer> getLetters() {
+        return letters;
+    }
+
+    /**
+     * getter numberOfAttempts
+     *
+     * @return numberOfAttempts
+     */
+    public int getNumberOfAttempts() {
+        return numberOfAttempts;
+    }
+
+    /**
+     * getter NUMBER_OF_LETTERS
+     *
+     * @return NUMBER_OF_LETTERS
+     */
+    public static int getLimitNumberOfLetters() {
+        return LIMIT_NUMBER_OF_LETTERS;
+    }
+
+    /**
+     * getter NUMBER_OF_ATTEMPTS
+     *
+     * @return NUMBER_OF_ATTEMPTS
+     */
+    public static int getLimitNumberOfAttempts() {
+        return LIMIT_NUMBER_OF_ATTEMPTS;
+    }
+
+    /**
+     * getter WORDLIST_FILE_PATH
+     *
+     * @return WORDLIST_FILE_PATH
+     */
+    public static String getWordlistFilePath() {
+        return WORDLIST_FILE_PATH;
+    }
+
+    /**
+     * getter SUCCESS_MESSAGE
+     *
+     * @return SUCCESS_MESSAGE
+     */
+    public static String getSuccessMessage() {
+        return SUCCESS_MESSAGE;
+    }
+
+    /**
+     * getter FAILURE_MESSAGE
+     *
+     * @return FAILURE_MESSAGE
+     */
+    public static String getFailureMessage() {
+        return FAILURE_MESSAGE;
+    }
+
+    /**
+     * Check inputWord is equal to answer.
+     *
+     * @param inputWord
+     * @return inputWord is equal to answer is true or false.
+     */
+    public boolean isAnswer(String inputWord) {
+        return inputWord.equals(answer);
+    }
+
+    /**
+     * setter outputText
+     * Set SUCCESS_MESSAGE or FAILURE_MESSAGE to outputText based on isSuccess.
+     *
+     * @param isSuccess
+     */
+    public void setOutputText(boolean isSuccess) {
+        outputText = isSuccess ? SUCCESS_MESSAGE + numberOfAttempts : FAILURE_MESSAGE + answer.toUpperCase();
+    }
+
+    /**
+     * setter numberOfAttempts
+     *
+     * @param numberOfAttempts
+     */
+    public void setNumberOfAttempts(int numberOfAttempts) {
+        this.numberOfAttempts = numberOfAttempts;
+    }
+
 }
--- wordle/src/test/java/stacs/wordle/WordleManagerTest.java
+++ wordle/src/test/java/stacs/wordle/WordleManagerTest.java
@@ -2,16 +2,190 @@ package stacs.wordle;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * WordleManagerTest
+ *
+ * @author Seigo
+ */
 class WordleManagerTest {
 
+    private static final String WORDLIST_TEST_FILE_PATH = "src/test/resources/wordlist-test.txt";
+    WordleManager wordleManager;
+
+    /**
+     * Initialize WordleManager before tests.
+     */
     @BeforeEach
-    void setUp() {
+    void setUp() throws FileNotFoundException {
+        wordleManager = new WordleManager(WORDLIST_TEST_FILE_PATH);
     }
 
     @AfterEach
     void tearDown() {
     }
+
+    /**
+     * Load a word list.
+     *
+     * @result test wordlist only contains 3 words, so wordlist should have the size of 3.
+     */
+    @Test
+    public void shouldLoadWordlist() throws FileNotFoundException {
+        ArrayList<String> wordlist = wordleManager.loadWordlist(WORDLIST_TEST_FILE_PATH);
+        assertEquals(3, wordlist.size());
+    }
+
+    /**
+     * Get a words.
+     *
+     * @result words should be set from WORDLIST_FILE_PATH after initialization in constructor.
+     */
+    @Test
+    public void shouldGetWords() throws FileNotFoundException {
+        ArrayList<String> words = wordleManager.loadWordlist(WORDLIST_TEST_FILE_PATH);
+        assertEquals(words, wordleManager.getWords());
+    }
+
+    /**
+     * Get an answer.
+     *
+     * @result answer should be set to one of the word randomly from WORDLIST_FILE_PATH after initialization in constructor.
+     */
+    @Test
+    public void shouldGetAnswer() throws FileNotFoundException {
+        assertTrue(
+                wordleManager.getAnswer().equals("MAVEN")
+                        || wordleManager.getAnswer().equals("DEBUG")
+                        || wordleManager.getAnswer().equals("CACHE")
+        );
+    }
+
+    /**
+     * Get a letters.
+     *
+     * @result letters should include one of 'E' char after initialization in constructor
+     * as all word in wordlist-test.txt include one of 'E'.
+     */
+    @Test
+    public void shouldGetLetters() {
+        char letter = 'E';
+        assertEquals(1, wordleManager.getLetters().get(letter));
+    }
+
+    /**
+     * Get an outputText.
+     *
+     * @result outputText should be equal to SUCCESS_MESSAGE + numberOfAttempts
+     * after called setOutputText with true.
+     */
+    @Test
+    public void shouldGetOutputTextSuccess() {
+        wordleManager.setNumberOfAttempts(3);
+        wordleManager.setOutputText(true);
+        assertEquals(wordleManager.getSuccessMessage() + wordleManager.getNumberOfAttempts(), wordleManager.getOutputText());
+    }
+
+    /**
+     * Get an outputText.
+     *
+     * @result outputText should be equal to FAILURE_MESSAGE + answer
+     * after called setOutputText with false.
+     */
+    @Test
+    public void shouldGetOutputTextFailure() {
+        wordleManager.setOutputText(false);
+        assertEquals(wordleManager.getFailureMessage() + wordleManager.getAnswer().toUpperCase(), wordleManager.getOutputText());
+    }
+
+    /**
+     * Get a LIMIT_NUMBER_OF_LETTERS.
+     *
+     * @result LIMIT_NUMBER_OF_LETTERS should be equal to 5.
+     */
+    @Test
+    public void shouldGetLimitNumberOfLetters() {
+        assertEquals(5, WordleManager.getLimitNumberOfLetters());
+    }
+
+    /**
+     * Get a LIMIT_NUMBER_OF_ATTEMPTS.
+     *
+     * @result LIMIT_NUMBER_OF_ATTEMPTS should be equal to 6.
+     */
+    @Test
+    public void shouldGetLimitNumberOfAttempts() {
+        assertEquals(6, WordleManager.getLimitNumberOfAttempts());
+    }
+
+    /**
+     * Get a WORDLIST_FILE_PATH.
+     *
+     * @result WORDLIST_FILE_PATH should be equal to "src/main/resources/wordlist.txt".
+     */
+    @Test
+    public void shouldGetWordListFilePath() {
+        assertEquals("src/main/resources/wordlist.txt", WordleManager.getWordlistFilePath());
+    }
+
+    /**
+     * Get a SUCCESS_MESSAGE.
+     *
+     * @result SUCCESS_MESSAGE should be equal to "Congratulations! Your score is ".
+     */
+    @Test
+    public void shouldGetSuccessMessage() {
+        assertEquals("Congratulations! Your score is ", WordleManager.getSuccessMessage());
+    }
+
+    /**
+     * Get a FAILURE_MESSAGE.
+     *
+     * @result FAILURE_MESSAGE should be equal to "Failure. The correct answer is ".
+     */
+    @Test
+    public void shouldGetFailureMessage() {
+        assertEquals("Failure. The correct answer is ", WordleManager.getFailureMessage());
+    }
+
+    /**
+     * Test inputWord is equal to one of the answer.
+     *
+     * @result should be true when isAnswer is called with the correct answer.
+     */
+    @Test
+    public void isAnswerTrue() {
+        String inputWord = wordleManager.getAnswer();
+        assertTrue(wordleManager.isAnswer(inputWord));
+    }
+
+    /**
+     * Test inputWord is not equal to one of the answer.
+     *
+     * @result should be false when isAnswer is called with the incorrect answer.
+     */
+    @Test
+    public void isAnswerFalse() {
+        String inputWord = "TESTS";
+        assertFalse(wordleManager.isAnswer(inputWord));
+    }
+
+    /**
+     * Get a numberOfAttempts.
+     *
+     * @result numberOfAttempts should be equal to 3
+     * after called setNumberOfAttempts with 3.
+     */
+    @Test
+    public void shouldGetNumberOfAttempts() {
+        wordleManager.setNumberOfAttempts(3);
+        assertEquals(3, wordleManager.getNumberOfAttempts());
+    }
+
 }
